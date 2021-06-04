d9538bcdc603f239fe36f1784453e45d5ac10de7
Furkan-Aoijs-Js <85354666+Furkan-Aoijs-Js@users.noreply.github.com>
2021-06-04 18:18:46 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+var dbd = require(`aoi.js`);
+var fs = require("fs");
+var bot = new aoi.Bot({
+  token: process.env.TOKEN,
+  prefix: "!"
+});
+
+bot.onMessage();
+bot.loadCommands(`./komutlar/`) ;
